6c552d7152aa7272de8ee9ebcf90a00ca4c130ba
zolynux <152818486+zolynux@users.noreply.github.com>
2024-10-20 09:27:22 -0500

    updated

---
+++ ejer_013_cadena_multilnes.py
@@ -0,0 +1,18 @@
+# Ejercicio 13: Crear una cadena de caracteres multi-línea.
+
+# cadena = "Python es un lenguaje de programación versátil y fácil de aprender."
+# cadena+= "Se usa para una variedad de aplicaciones, desde desarrollo web"
+# cadena+= "hasta ciencia de datos e inteligencia artificial. Su sintaxis clara y legible"
+# cadena+= "lo hace popular tanto entre principiantes como expertos."
+
+cadena = """Python es un lenguaje de programación versátil y fácil de aprender.
+Se usa para una variedad de aplicaciones, desde desarrollo web
+hasta ciencia de datos e inteligencia artificial. Su sintaxis clara y legible
+lo hace popular tanto entre principiantes como expertos."""
+
+
+print(cadena)
+
+cadena = "Python es un lenguaje de programación versátil y fácil de aprender. Se usa para una variedad de aplicaciones, desde desarrollo web hasta ciencia de datos e inteligencia artificial. Su sintaxis clara y legible lo hace popular tanto entre principiantes como expertos."
+
+print(cadena)
