64d84b84b29fdedd099931f728a6c8169feccd5e
Zolyn Linux <152818486+zolynux@users.noreply.github.com>
2024-10-22 15:28:36 +0000

    updated

---
+++ ejer_019_validacion_cadena.py
@@ -0,0 +1,17 @@
+# Ejercicio 19: Comprobar si una cadena termina con la extensión .py, sino es asi, agregar la extensión.
+
+# main.py
+# modulo => modulo.py
+
+def validar_nombre_archivo(nombre_archivo):
+    """
+    Valida si un nombre de archivo tiene la extensión .py
+    Si el archivo no tiene la extensión, la agrega.
+    """
+    if len(nombre_archivo) >= 3 and nombre_archivo[-3:] == '.py':
+        return nombre_archivo
+    
+    return nombre_archivo + '.py'
+
+print(validar_nombre_archivo('main.py'))
+print(validar_nombre_archivo('modulo'))
